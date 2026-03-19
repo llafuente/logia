526c19cf8dc2cceafd5eb9ce415e8b69e1106cfa
llafuente <luis.lafuente.morales@gmail.com>
2026-03-19 23:49:45 +0100

    feat: start working in comptime ABI

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,16 @@
 #include <cstdlib>
 #include <csignal>
 
+// Define export/import macros for Windows
+#ifdef _WIN32
+    #define LOGIA_EXPORT __declspec(dllexport)
+    #define LOGIA_API extern "C" LOGIA_EXPORT
+#else
+    // On Linux/macOS, use GCC visibility attributes
+    #define LOGIA_EXPORT __attribute__((visibility("default")))
+    #define LOGIA_API extern "C" LOGIA_EXPORT
+#endif
+
 void print_stack_trace();
 
 // Custom assert macro with stack trace
