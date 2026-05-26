b1b4be66e1778f7dff0327990357094cc2f4900b
llafuente <luis.lafuente.morales@gmail.com>
2026-05-26 17:12:02 +0200

    feat: start cast node

---
+++ src/logia/ast/cast.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "logia/ast/expr.h"
+
+namespace logia::AST
+{
+    /// @brief Defines a cast expression
+    struct Cast : Expression
+    {
+        /// @brief Some casting operations are in fact a function
+        CallExpression *callexpr = nullptr;
+
+        Cast(
+            antlr4::ParserRuleContext *rule,
+            Expression *expr,
+            TypeDef *to);
+        Type *get_from_type();
+        Expression *get_expr();
+        Type *get_to_type();
+
+        std::string to_string() override;
+        Type *get_type() override;
+
+        void post_codegen(logia::Backend *backend) override;
+
+    protected:
+        void _post_type_inference() override;
+    };
+}
